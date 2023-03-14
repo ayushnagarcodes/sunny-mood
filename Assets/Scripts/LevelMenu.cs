102311a04fb0b42845086c9082be4b82ec5d69f3
Ayush Nagar <ayushnagar15tfe@gmail.com>
2023-03-14 11:27:57 +0530

    Stage6 Completed

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -9,27 +9,47 @@ using UnityEngine.UI;
 public class LevelMenu : MonoBehaviour
 {
     private Button[] btns;
-    private String btnName;
+    // private String btnName;
     
     private void Start()
     {
         btns = GetComponentsInChildren<Button>();
-
+    
         for (int i = 0; i <= PlayerPrefs.GetInt("LevelCompleted"); i++)
         {
             btns[i].interactable = true;
         }
-
-        for (int i = 0; i < btns.Length - 1; i++)
-        {
-            btns[i].onClick.AddListener(StartLevel);
-        }
+        
+        // for (int i = 0; i < btns.Length - 1; i++)
+        // {
+        //     btns[i].onClick.AddListener(StartLevel);
+        // }
     }
+    
+    // public void StartLevel()
+    // {
+    //     btnName = EventSystem.current.currentSelectedGameObject.name;
+    //     PlayerPrefs.SetInt("CurrentLevel", Int32.Parse(btnName[^1].ToString()));
+    //     SceneManager.LoadScene(btnName);
+    // }
 
-    public void StartLevel()
+    // Implementing using this is inefficient as there can be 100 levels which means 300 lines of repetition
+    // So, instead uncomment the code above and comment the below code
+    public void Level1()
     {
-        btnName = EventSystem.current.currentSelectedGameObject.name;
-        PlayerPrefs.SetInt("CurrentLevel", Int32.Parse(btnName[^1].ToString()));
-        SceneManager.LoadScene(btnName);
+        PlayerPrefs.SetInt("CurrentLevel", 1);
+        SceneManager.LoadScene("Level 1");
+    }
+    
+    public void Level2()
+    {
+        PlayerPrefs.SetInt("CurrentLevel", 2);
+        SceneManager.LoadScene("Level 2");
+    }
+    
+    public void Level3()
+    {
+        PlayerPrefs.SetInt("CurrentLevel", 3);
+        SceneManager.LoadScene("Level 3");
     }
 }
